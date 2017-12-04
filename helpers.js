090dfab9a321cac26584b5f3f2252b0182d6259e
Sergey Badulin <sbadulin@gmail.com>
2017-12-04 23:16:39 +0300

    перевод helper.js

--- helpers.js
+++ helpers.js
@@ -1,24 +1,24 @@
 /*
-  This is a file of data and helper functions that we can expose and use in our templating function
+  Здесь находятся функции-хелперы, которые мы используем в шаблонах
 */
 
-// FS is a built in module to node that let's us read files from the system we're running on
+// встроенный в node модуль, который позволяет читать файлы из системы на которой запущен
 const fs = require('fs');
 
-// moment.js is a handy library for displaying dates. We need this in our templates to display things like "Posted 5 minutes ago"
+// moment.js это библиотека для отображения дат. Нужна в шаблонах для показа вещей типа "Опубликовано 5 минут назад"
 exports.moment = require('moment');
 
-// Dump is a handy debugging function we can use to sort of "console.log" our data
+// Dump используем для отладки, наподобие console.log
 exports.dump = (obj) => JSON.stringify(obj, null, 2);
 
-// Making a static map is really long - this is a handy helper function to make one
+// Этот хелпер упрощает создание карты через Google Static Maps API
 exports.staticMap = ([lng, lat]) => `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=14&size=800x150&key=${process.env.MAP_KEY}&markers=${lat},${lng}&scale=2`;
 
-// inserting an SVG
+// вставка svg-файлов 
 exports.icon = (name) => fs.readFileSync(`./public/images/icons/${name}.svg`);
 
-// Some details about the site
-exports.siteName = `Now That's Delicious!`;
+// Подробности о сайте
+exports.siteName = `Все компании Клиентикс`;
 
 exports.menu = [
   { slug: '/stores', title: 'Stores', icon: 'store', },
